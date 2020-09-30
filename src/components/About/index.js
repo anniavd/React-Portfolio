a97436ee7a12b89594fd22d3d0c13e5395c18957
annia valdes <millerl.mb79@gmail.com>
2020-09-30 14:26:38 -0500

    create a About component and add to App.js

---
+++ src/components/About/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+function About(){
+    return(
+        <section>
+            <h1 id="about">About</h1>
+            <p id= "textAbout"> My name is Annia Valdes Diaz. I am a graduate in Computer Science Engineering since 2008. 
+                         I have more than 5 years of experience worked with software development team.
+                         I worked in software development projects at the University of Computer Sciences (UCI ).
+                         During this time I assumed the roles like a Principal System Analyst, Project Leader,
+                        Unit Test Designer for the software and Software Quality Control.My new skills as a developer are: HTML/CSS, 
+                        SQL, NoSQL, MongoDB, Node.js, Express, ApisResful and React.                         
+         </p>
+        </section>
+    );
+}
+
+export default About;
