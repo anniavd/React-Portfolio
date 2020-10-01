05910ed980cf290a7d0cbcb85f20f5640ce328b7
annia valdes <millerl.mb79@gmail.com>
2020-10-01 11:27:55 -0500

    add a Nav component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import About from './components/About';
+import Nav from './components/Nav';
 //import './App.css';
 
 function App() {
+
+  const [categories] = useState([
+    
+    { name: "About me"},        
+    { name: "Portfolio"},
+     { name: "Contact"},
+     { name: "Resume"},        
+     
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [contactSelected, setContactSelected] = useState(false); //single-page application
+
+
+
   return (
     <div >
-     <main>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
+     <main>      
        <About></About>
      </main>
     </div>
