5629acab5878484ebd8dcc9b960b76f84772b39b
annia valdes <millerl.mb79@gmail.com>
2020-10-02 09:48:05 -0500

    update the way to render the navegation

--- src/App.js
+++ src/App.js
@@ -1,38 +1,40 @@
 import React, { useState } from 'react';
 import About from './components/About';
-import Nav from './components/Nav';
+import Navegation from './components/Navegation';
 import Footer from './components/Footer';
 import Contact from './components/Contact';
 
 
 function App() {
 
-  const [categories] = useState([
+ /* const [categories] = useState([
     
     { name: "About me"},        
     { name: "Portfolio"},
      { name: "Contact"},
      { name: "Resume"},        
      
-  ]);
+  ]);*/
+  
+ 
 
-  const [currentCategory, setCurrentCategory] = useState(categories[0]);
-  const [contactSelected, setContactSelected] = useState(false); //single-page application
+  //const [currentCategory, setCurrentCategory] = useState(categories[0]);
+ // const [contactSelected, setContactSelected] = useState(false); //single-page application
 
-
+const [currentPage,setCurrentPage]=useState('About')
+  const renderPage=()=>{
+    switch(currentPage){
+     // case 'Portfolio': return <Portfolio/>;
+      case 'Contact':return <Contact/>;
+     // case 'Resume': return <Resume/>;
+      default: return <About/>
+    }
+  }
 
   return (
-    <div >
-      <Nav
-        categories={categories}
-        setCurrentCategory={setCurrentCategory}
-        currentCategory={currentCategory}
-        contactSelected={contactSelected}
-        setContactSelected={setContactSelected}
-      ></Nav>
-     <main>             
-       <About></About>
-     </main>
+    <div>
+     <Navegation currentPage={currentPage} setCurrentPage={setCurrentPage}/>
+     {renderPage()}
      <Footer></Footer>
     </div>
   );
