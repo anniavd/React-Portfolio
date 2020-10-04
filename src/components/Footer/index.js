a1b030f802016107100bd6dec377368ea2a55159
annia valdes <millerl.mb79@gmail.com>
2020-10-04 12:28:19 -0500

    update code

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faLinkedin} from '@fortawesome/free-brands-svg-icons';
-import{ faGithubSquare} from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithubSquare } from '@fortawesome/free-brands-svg-icons';
 
 
 
@@ -10,16 +10,16 @@ import{ faGithubSquare} from '@fortawesome/free-brands-svg-icons';
 function Footer() {
     return (
         <section>
-            <div class=" footer">
-            <h2>Contact Info</h2>
-            <p>346-314-xxxx</p>
-            <p>annia.valdesd@gmail.com</p>
-             <a href=" https://www.linkedin.com/in/annia-valdes-diaz-77b2591a8/" target="_blank" style={{fontSize:'40px',color:'orange'}}><FontAwesomeIcon icon={faLinkedin}  /></a>  
+            <div className="footer">
+                <h2>Contact Info</h2>
+                <p>346-314-xxxx</p>
+                <p>annia.valdesd@gmail.com</p>
+                <a href=" https://www.linkedin.com/in/annia-valdes-diaz-77b2591a8/" target="_blank" ><FontAwesomeIcon icon={faLinkedin} /></a>
 
-              <a href=" https://github.com/anniavd?tab=repositories" target="_blank" style={{fontSize:'40px',color:'orange'}}><FontAwesomeIcon icon={faGithubSquare} /> </a>
+                <a href=" https://github.com/anniavd?tab=repositories" target="_blank"><FontAwesomeIcon icon={faGithubSquare} /> </a>
 
-   </div>
- </section>
+            </div>
+        </section>
     )
 }
 
