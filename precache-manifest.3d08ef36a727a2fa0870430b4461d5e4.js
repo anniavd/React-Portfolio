48f98fbc5b117aa2b90580f09b3ecfb3cc3978f3
annia valdes <millerl.mb79@gmail.com>
2020-10-09 10:22:31 -0500

    Updates

---
+++ precache-manifest.3d08ef36a727a2fa0870430b4461d5e4.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f90747869687b1385ae97cdeaeac936",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "c19dd2a3b9489cbad802",
+    "url": "/react-portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "26f5fd8206b1e4d10242",
+    "url": "/react-portfolio/static/css/main.7c36acbd.chunk.css"
+  },
+  {
+    "revision": "c19dd2a3b9489cbad802",
+    "url": "/react-portfolio/static/js/2.6f97b701.chunk.js"
+  },
+  {
+    "revision": "fee3ef41109770b3c68abe9f8e3cdf99",
+    "url": "/react-portfolio/static/js/2.6f97b701.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26f5fd8206b1e4d10242",
+    "url": "/react-portfolio/static/js/main.e35c1895.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "da56b3564496a26fd742749ab2d33c42",
+    "url": "/react-portfolio/static/media/ann.da56b356.jpg"
+  },
+  {
+    "revision": "192bcf58b2f1f1e3c52c8d6cd4e3cdf6",
+    "url": "/react-portfolio/static/media/e-commerce.192bcf58.jpg"
+  },
+  {
+    "revision": "b26fce140b539630ebe93da7756bb390",
+    "url": "/react-portfolio/static/media/employee.b26fce14.jpg"
+  },
+  {
+    "revision": "b08042fe70ccfa1fbaef246dc5e9ca8e",
+    "url": "/react-portfolio/static/media/instartist.b08042fe.jpg"
+  },
+  {
+    "revision": "9bce7ce937ac3e396d45e5508fbc47b9",
+    "url": "/react-portfolio/static/media/note-taker.9bce7ce9.jpg"
+  },
+  {
+    "revision": "bed4a5dd68199668d08d717bf84dbf28",
+    "url": "/react-portfolio/static/media/password.bed4a5dd.jpg"
+  },
+  {
+    "revision": "88ac313d291ea971614486cf306e8018",
+    "url": "/react-portfolio/static/media/project1.88ac313d.png"
+  },
+  {
+    "revision": "f48d398543cf9d9b700cb8dfb3e96205",
+    "url": "/react-portfolio/static/media/scheduler.f48d3985.jpg"
+  },
+  {
+    "revision": "a92d965b94c6aec77e75c56dec826ec5",
+    "url": "/react-portfolio/static/media/screenshot.a92d965b.jpg"
+  },
+  {
+    "revision": "f0a78d1e921d8abcefcc2d508fb56cde",
+    "url": "/react-portfolio/static/media/team-members.f0a78d1e.jpg"
+  },
+  {
+    "revision": "8e12f3682ab591e5ab207dbe38ebe9d1",
+    "url": "/react-portfolio/static/media/weather.8e12f368.jpg"
+  }
+]);
