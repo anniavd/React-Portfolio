1c2c483b80d77956d49e31248c334b1e6a11267d
annia valdes <millerl.mb79@gmail.com>
2021-03-05 15:32:54 -0600

    add more skills

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import  Skills from '../skills/index';
+import Skills from '../skills/index';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFile } from '@fortawesome/free-solid-svg-icons';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 //<i class="fas fa-file-alt"></i>
 
 function Resume() {
 
-   const others=['Analysis and Design of the Software'];
+	const others = ['Analysis and Design of the Software',
+		'Git',
+		'Github',
+		'Heroku',
+		' Unit Test Design',
+		'Microsoft Word',
+		'Excel',
+		'Power Point',
+		'Photoshop',
+		'ie.zoom',
+		'slack',
+		'Google Duo',
+		' Hangouts',
+		'Screencastify'
+	];
 
 	const front = [
 		'HTML 5',
 		'CSS 3',
 		'jQuery',
 		'Javascript',
-		'React',		
+		'React',
 		'Bootstrap',
-		'Materialize',		
-		'Responsive design',
-		'ApisResful',
-                
+		'Handlebars',		
+		'Materialize',
+		'Responsive Design',
+		'Apis Resful',
+		'Jest'
 	];
 
 	const back = [
@@ -29,25 +46,42 @@ function Resume() {
 		'Sequelize ORM',
 		'MongoDB',
 		'Mongoose',
-		
-    ];
-    
+		'GraphQL',
+		'APIS',
+		'JSON',
+		'JWT',
+		'Stripe',
+		'Bcrypt',
+		'Cloudinary',
+		'Multer',
+
+	];
+
 
 	return (
-        <main>
-		<div className="resume" >
-			<h2>
-				See the Resume {' '}
-				<a	href="https://docs.google.com/document/d/1gQVMJ_44nAI9LxOkditZbVPTkwUpZovGTGVlQDBMLgw/edit?usp=sharing"   rel="noopener noreferrer"	target="_blank"	><FontAwesomeIcon icon={faFile} /></a>
-			</h2>
-			<h3>Front-end Skills</h3>
-			<ul  className="elemtSkill">{front.map((fbend) => <Skills  fbend={fbend} key={fbend}/>)}</ul>	
-			<h3>Back-end Skills</h3>
-			<ul>{back.map((fbend) => <Skills fbend={fbend} key={fbend}  />)}</ul>	
-			<h3>Others Skills</h3>
-			<ul>{others.map((fbend) => <Skills fbend={fbend} key={fbend}  />)}</ul>	
-		</div>
-        </main>
+		<main>
+			<div className="resume" >
+				<h2>
+					See the Resume {' '}
+					<a href="https://docs.google.com/document/d/1gQVMJ_44nAI9LxOkditZbVPTkwUpZovGTGVlQDBMLgw/edit?usp=sharing" rel="noopener noreferrer" target="_blank"	><FontAwesomeIcon icon={faFile} /></a>
+				</h2>
+				<Row >
+					<Col sm>
+						<h3>Front-end Skills</h3>
+						<ul className="elemtSkill">{front.map((fbend) => <Skills fbend={fbend} key={fbend} />)}</ul>
+					</Col>
+					<Col sm>
+						<h3>Back-end Skills</h3>
+						<ul>{back.map((fbend) => <Skills fbend={fbend} key={fbend} />)}</ul>
+					</Col>
+					<Col sm>
+						<h3>Others Skills</h3>
+						<ul>{others.map((fbend) => <Skills fbend={fbend} key={fbend} />)}</ul>
+					</Col>
+				</Row >
+
+			</div>
+		</main>
 	);
 }
 
